09b0353fe996218bddda9c577be4608c11ca34d3
Edman <edmancota2017@gmail.com>
2020-02-27 10:15:02 -0600

    Agregar Turret
    Agregar Turret

---
+++ Proyecto/Assets/Scripts/PlayerNavigation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerNavigation : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public GameObject[] waypoints;
+
+    private int currWP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.transform.GetChild(0);
+
+     agent = GetComponent<NavMeshAgent>();
+
+        if (agent && waypoints.Length > 0) agent.SetDestination(waypoints[currWP].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent)
+        {
+            if(agent.remainingDistance < 0.2f)
+            {
+                currWP++;
+                currWP %= waypoints.Length;
+                agent.SetDestination(waypoints[currWP].transform.position);
+            }
+        }
+    }
+}
